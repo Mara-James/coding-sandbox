4e5c0b2b804cde7f96c845b619bdec6404dd445b
Mara Cunningham <mara.james.codes@gmail.com>
2023-03-29 09:36:57 -0500

    Using Dictionaries instead of Lists

--- Projects/Mara-Prompts/Prompt.py
+++ Projects/Mara-Prompts/Prompt.py
@@ -6,17 +6,26 @@ class Prompt:
         self.nickname=nickname
         self.prompt=prompt
         
-
+    
     def order(self,*words):
 
+        # prompt_entries=[]
+
+        # for entry in words:
+        #     entry= input(f"Please enter a(n) {entry}:\n")
+        #     print (entry)
+        #     prompt_entries.append(entry)
+        # print(prompt_entries)
+        
+        self.prompt_entries= {}
+            
         for entry in words:
-            entry= input(f"Please enter a(n) {entry}:\n")
-            print(entry)
-            
-            
+          value= input(f"Please enter a(n) {entry}:\n")
+          self.prompt_entries[entry]= value
+        
+        print(self.prompt_entries)
+        ####    condsider making genre into a global list instead of a class of its own
 
-
-
-first_try=Prompt("Prompt 1","Some days when the weather was 1 , Kendle enjoyed 2 through the local 3.")
+first_try=Prompt("Prompt 1","Some days when the weather was _{}_ , Kendle enjoyed _{}_ through the local _{}_.")
 
 first_try.order('adj', 'verb', 'noun','adj2')
