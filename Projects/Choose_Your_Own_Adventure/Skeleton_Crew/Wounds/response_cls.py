8c23c6a663ac4bf742b15ffd6ba4d86f7f2c68a0
Mara Cunningham <mara.james.codes@gmail.com>
2023-06-14 15:48:32 -0500

    database refactor to use dictionaries over variables

--- Projects/Choose_Your_Own_Adventure/Skeleton_Crew/Wounds/response_cls.py
+++ Projects/Choose_Your_Own_Adventure/Skeleton_Crew/Wounds/response_cls.py
@@ -17,31 +17,39 @@ class Response():
 
     
     
-### Sceme 1 response variables
+### Sceme 1 response options
 scene_one= Response("Wait or Sail", 1)
-s1_wait1= "wait 1 week for fletcher" # -1000 -> the original amount of money the player has is 4000
-s1_wait2= "wait 2 weeks for fletcher" # -2000
-s1_wait3= "wait three weeks for fletcher"# -3000
-s1_sail= "dont wait for fletcher"
+scene_1_options = {
+    "1": "wait 1 week for fletcher", # -1000 -> the original amount of money the player has is 4000 
+    "2": "wait 2 weeks for fletcher", # -2000
+    "3": "wait three weeks for fletcher", # -3000
+    "4": "dont wait for fletcher"
+    }
 
 scene_one_a= Response("wait for Fletcher",1)
-s1_lore1_qa= "lore dump 1"
-s1_lore2__qa= "lore dump 2"
-s1_lore3_qa= "lore dump 3"
-s1_lore4_qa= "lore dump 4"
-### if user asks all questions they recieve fletcher's telescope and a satchel with 5000 if they waited 3 weeks
+### this will need to be part of a while loop that allows users to ask questions 1-4 or skip to the final question
+lore_options= {
+    "1": "lore dump 1",
+    "2": "lore dump 2",
+    "3": "lore dump 3",
+    "4": "lore dump 4",
+    "5": "closing question"
+}
+### if user asks all questions they recieve fletcher's telescope (and a satchel with 5000 if they waited 3 weeks)
 
 
 
 ### Scene 2 response variables
 scene_two = Response("Crew or Solo", 2)
-s2_solo = "solo"
-s2_crew_one = "1000 crew"
-s2_crew_two = "2000 crew"
-s2_crew_three = "3000 crew"
-s2_crew_four= "4000 crew"
-s2_crew_five= "5000 crew"
-s2_crew_six= "6000 crew"
+scene_2_options= {
+    "0": "solo",
+    "1": "1000 crew",
+    "2": "2000 crew",
+    "3": "3000 crew",
+    "4": "4000 crew",
+    "5": "5000 crew",
+    "6": "6000 crew"
+}
 ##### make it so the better the crew you pay for the less damage your base line is
 #### danger break down is as follows
 #       solo- damage is 6x normal, the stakes are a lot higher you have misakes that cost between 6 and 18 hp
@@ -56,76 +64,103 @@ s2_crew_six= "6000 crew"
 
 ### Scene 3 response variables
 scene_three= Response("Pirate Attack", 3)
-s3_safe = "safe"
-s3_answer_one = "wound 1"
-s3_answer_two = "wound 2"
-s3_answer_three = "wound 3"
-s3_perish = "perish" 
+scene_3_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 4 response variables
 scene_four= Response("Eye of the Storm", 4)
-s4_safe = "safe"
-s4_answer_one = "wound 1"
-s4_answer_two = "wound 2"
-s4_answer_three = "wound 3"
-s4_perish = "perish" 
+scene_4_options={
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+}
 
 ### Scene 5 response variables
 scene_five_a= Response("As luck would have it", 5)
 s5a_safe = "safe"
 
 scene_five_b= Response("A Frightful Mistake", 5)
-s5b_safe = "safe"
-s5b_answer_one = "wound 1"
-s5b_answer_two = "wound 2"
-s5b_answer_three = "wound 3"
-s5b_perish = "perish" 
+scene_5b_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 6 response variables 
-s6_safe = "safe"
-s6_answer_one = "wound 1"
-s6_answer_two = "wound 2"
-s6_answer_three = "wound 3"
-s6_perish = "perish" 
+scene_six = Response("Bone to Pick", 6)
+scene_6_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 7 response variables
-s7_safe = "safe"
-s7_answer_one = "wound 1"
-s7_answer_two = "wound 2"
-s7_answer_three = "wound 3"
-s7_perish = "perish" 
+scene_seven = Response("A Cryptic Map", 7)
+scene_7_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 8 response variables
-s8_safe = "safe"
-s8_answer_one = "wound 1"
-s8_answer_two = "wound 2"
-s8_answer_three = "wound 3"
-s8_perish = "perish" 
+scene_eight = Response("The Trees have Eyes", 8)
+scene_8_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 9 response variables
-s9_safe = "safe"
-s9_answer_one = "wound 1"
-s9_answer_two = "wound 2"
-s9_answer_three = "wound 3"
-s9_perish = "perish" 
+scene_nine= Response("Hels's Cave", 9)
+scene_9_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 10 response variables
-s10_safe = "safe"
-s10_answer_one = "wound 1"
-s10_answer_two = "wound 2"
-s10_answer_three = "wound 3"
-s10_perish = "perish" 
+scene_ten= Response("Stalagmite Labyrinth", 10)
+scene_10_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 11 response variables
-s11_safe = "safe"
-s11_answer_one = "wound 1"
-s11_answer_two = "wound 2"
-s11_answer_three = "wound 3"
-s11_perish = "perish" 
+scene_eleven= Response("Warm Welcome", 11)
+scene_11_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
 
 ### Scene 12 response variables
-s12_safe = "safe"
-s12_answer_one = "wound 1"
-s12_answer_two = "wound 2"
-s12_answer_three = "wound 3"
-s12_perish = "perish" 
+scene_tweleve= Response("The Gift", 12)
+scene_12_options= {
+    "1":"safe",
+    "2":"wound 1",
+    "3":"wound 2",
+    "4":"wound 3",
+    "5":"perish",
+    }
